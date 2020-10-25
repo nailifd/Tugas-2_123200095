5cbcf722ebd9fcb1b7f95d1e130d982a9a154b1f
nailifd <73066418+nailifd@users.noreply.github.com>
2020-10-26 04:18:25 +0700

    Add files via upload

---
+++ Tugas2_123200095.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{	
+	int umur, Rp, bantuan, hari, ayah, ibu, anak1, anak2;
+	float 
+	string
+	;cout<<"Umur ayah :";
+	;cin>>ayah;
+	;cout<<"Umur ibu :";
+	cin>>ibu;
+	cout<<"Umur anak 1 :";
+	cin>>anak1;
+	cout<<"Umur anak 2 :";
+	cin>>anak2;
+	
+	if (umur<=12){bantuan ;Rp;15000*30;
+	}
+	if (umur=12-20){bantuan ;Rp;20000*30;
+	}
+	if (umur>=20){bantuan ;Rp;30000*30;
+	}
+	
+	cout<<((15000*30)+(20000*30)+(30000*30)+(30000*30));
+	cout<<endl;
+}
